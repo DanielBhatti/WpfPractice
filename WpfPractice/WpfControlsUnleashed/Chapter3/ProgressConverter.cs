0b36d4651c3b160def1e3533de18e00203b376e2
Daniel Bhatti <danielbbhatti@gmail.com>
2021-06-19 23:17:33 -0400

    Trying out some of the book examples.

---
+++ WpfPractice/WpfControlsUnleashed/Chapter3/ProgressConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace WpfPractice.WpfControlsUnleashed
+{
+    public class ProgressConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            double progress = (double)values[0];
+            ProgressBar progressBar = values[1] as ProgressBar;
+            return 359.999 * (progress / (progressBar.Maximum - progressBar.Minimum));
+            /*
+            One thing that might look odd is the use of 359.9999 as the destination angle instead of
+            360. This is a quirk of modulo arithmetic. When code, including WPF (but not limited to
+            WPF, I’ve seen this in several animation frameworks), considers drawing from 0 to 360,
+            since 360 is actually at the same location as 0, “optimizations” often decide to do no
+            drawing at all. So to force the drawing to take place, we use “almost 360.” 
+            */
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfPractice/WpfControlsUnleashed/Chapter4/ZOrderControl.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace WpfPractice.WpfControlsUnleashed
+{
+	[ContentProperty("Children")]
+	public class ZOrderControl : FrameworkElement
+	{
+		private readonly Size ChildSize = new Size(200, 100);
+
+		public bool IsOrderReversed
+		{
+			get { return (bool)GetValue(IsOrderReversedProperty); }
+			set { SetValue(IsOrderReversedProperty, value); }
+		}
+
+		public UIElementCollection Children { get; set; } 
+
+		protected override int VisualChildrenCount
+		{
+			get { return Children.Count; }
+		}
+
+		public int Offset { get; set; } = 50;
+
+		public static readonly DependencyProperty IsOrderReversedProperty = DependencyProperty.
+			Register(
+			"IsOrderReversed", typeof(bool), typeof(ZOrderControl),
+			new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.AffectsArrange,
+										  OnIsOrderReversedChanged));
+
+		public ZOrderControl()
+        {
+			Children = new UIElementCollection(this, this);
+		}
+
+		private static void OnIsOrderReversedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			ZOrderControl control = d as ZOrderControl;
+			Reparent(control);
+		}
+
+		private static void Reparent(ZOrderControl control)
+		{
+			for (int i = 0; i < control.Children.Count; i++)
+			{
+				control.RemoveVisualChild(control.Children[i]);
+			}
+
+			for (int i = 0; i < control.Children.Count; i++)
+			{
+				control.AddVisualChild(control.Children[i]);
+			}
+		}
+
+		protected override Size MeasureOverride(Size constraint)
+		{
+			if (constraint.Width == double.PositiveInfinity || constraint.Height == double.PositiveInfinity)
+				return Size.Empty;
+
+			for (int i = 0; i < Children.Count; i++)
+			{
+				Children[i].Measure(ChildSize);
+			}
+
+			return constraint;
+		}
+
+		protected override Size ArrangeOverride(Size finalSize)
+		{
+			for (int index = 0; index < Children.Count; index++)
+			{
+				Children[index].Arrange(new Rect(new Point(index * Offset, 0), ChildSize));
+			}
+
+			return finalSize;
+		}
+
+		protected override Visual GetVisualChild(int index)
+		{
+			if (index < 0 || index >= Children.Count)
+			{
+				throw new Exception("Bad Index");
+			}
+
+			// Works well for load-time
+			// Reverse the Z order
+			if (IsOrderReversed)
+			{
+				return Children[Children.Count - 1 - index];
+			}
+
+			// Normal Z order
+			return Children[index];
+		}
+	}
+}
